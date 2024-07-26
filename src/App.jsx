2bf0ed93eca9313a8b2901e044ab4b59c3e011b3
May Thu Nguyen <thumay@Thus-MacBook-Air.local>
2024-07-26 19:02:26 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Features from "./components/Features";
+import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
+import Model from "./components/Model"
+import Navbar from "./components/Navbar"
+
+
+const App = () => {
+
+
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+    <Highlights/>
+    <Model />
+    <Features/>
+
+    </>
+    
+  )
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), sentryVitePlugin({
+    org: "may-mjs",
+    project: "javascript-react"
+  }), sentryVitePlugin({
+    org: "may-mjs",
+    project: "javascript-react"
+  }), sentryVitePlugin({
+    org: "may-mjs",
+    project: "javascript-react"
+  }), sentryVitePlugin({
+    org: "may-mjs",
+    project: "javascript-react"
+  }), sentryVitePlugin({
+    org: "may-mjs",
+    project: "javascript-react"
+  }), sentryVitePlugin({
+    org: "may-mjs",
+    project: "javascript-react"
+  }), sentryVitePlugin({
+    org: "may-mjs",
+    project: "javascript-react"
+  }), sentryVitePlugin({
+    org: "may-mjs",
+    project: "javascript-react"
+  })],
+
+  build: {
+    sourcemap: true
+  }
+})
